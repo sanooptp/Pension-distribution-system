471d3a0e902436b8ec88f019135d33621d552248
meriza-techversant <meriza@techversantinfo.com>
2021-12-28 18:41:47 +0530

    registration

--- Pension/user_profile/models.py
+++ Pension/user_profile/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+
+class ExtendedUserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,10}$',
+                                 message="Phone number must be entered in the format +919999999999. Up to 10 digits allowed.")
+    phone = models.CharField('Phone', validators=[phone_regex], max_length=10, unique=True, null=True)
+
+
+    def __str__(self):
+        return str(self.id)
+
+
--- Pension/user_profile/views.py
+++ Pension/user_profile/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import RegistrationSerializer
+from rest_framework.generics import GenericAPIView
 
-# Create your views here.
+
+class RegisterView(GenericAPIView):
+    serializer_class = RegistrationSerializer
+
+    def post(self,request):
+        serializer =RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            user = serializer.save()
+            data['response'] = "successfully registered a new user"
+
+        else:
+            data = serializer.errors
+        return Response(data)
+
