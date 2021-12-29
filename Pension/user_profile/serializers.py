9e46eaced3421ed5f8e16e1180375fa8c9eca8cc
meriza-techversant <meriza@techversantinfo.com>
2021-12-29 11:12:13 +0530

    serializer

--- Pension/user_profile/serializers.py
+++ Pension/user_profile/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-<<<<<<< HEAD
+
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
